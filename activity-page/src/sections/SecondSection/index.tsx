d19ea56e4a26684b95469f9cb4b811c1afaa71e6
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-12-08 13:29:39 -0500

    basic structure of second section

--- activity-page/src/sections/SecondSection/index.tsx
+++ activity-page/src/sections/SecondSection/index.tsx
@@ -4,10 +4,41 @@
 
 // };
 
+import CartoonImage from '../../assets/cartoon.jpg'
+import FoodImage from '../../assets/food.jpg'
+import LifeImage from '../../assets/life.jpg'
+import MovieImage from '../../assets/movie.png'
+
 function SecondSection() {
   return (
     <div>
-      Tabs + tabs content
+      {/* tabs */}
+      <ul>
+        <li>Cartoon</li>
+        <li>Food</li>
+        <li>Movie</li>
+        <li>Life</li>
+      </ul>
+
+      {/* tab content */}
+      <div>
+        <section>
+          <h2>Cartoon</h2>
+          <img src={CartoonImage} alt='Cartoon image' />
+        </section>
+        <section>
+          <h2>Food</h2>
+          <img src={FoodImage} alt='Food image' />
+        </section>
+        <section>
+          <h2>Movie</h2>
+          <img src={MovieImage} alt='Movie image' />
+        </section>
+        <section>
+          <h2>Life</h2>
+          <img src={LifeImage} alt='Life image' />
+        </section>
+      </div>
     </div>
   )
 }
