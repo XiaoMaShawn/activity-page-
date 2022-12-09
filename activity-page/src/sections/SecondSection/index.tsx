24f44fd419feafc6e6d0cf1188af49aa8ed76340
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-12-09 09:26:27 -0500

    fix the tab when u scroll the page

--- activity-page/src/sections/SecondSection/index.tsx
+++ activity-page/src/sections/SecondSection/index.tsx
@@ -4,7 +4,7 @@
 
 // };
 
-import { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import CartoonImage from '../../assets/cartoon.jpg'
 import FoodImage from '../../assets/food.jpg'
 import LifeImage from '../../assets/life.jpg'
@@ -34,19 +34,39 @@ const tabs = [
 
 function SecondSection() {
   const [activeTab, setActiveTab] = useState<string>('cartoon');
+  const [isFixed, setIsFixed] = useState(false);
+
+  const secondSectionRef = useRef<HTMLDivElement>(null);//must have initial value null
 
   const activate = (key: string) => {
     setActiveTab(key)
-    const tabContentEl = document.querySelector(`[data-id=${key}]`);//care about the usage about [] and `` in querySelector
+    const tabContentEl = document.querySelector(`[data-id=${key}]`);//care about the usage of [] and `` in querySelector argument
     if (tabContentEl) {
       tabContentEl.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
+  const onScroll = () => {
+    if (secondSectionRef.current) {
+      //get the top value of secondSection
+      const { top } = secondSectionRef.current.getBoundingClientRect();
+      //fix the tab if top == 0
+      setIsFixed(top <= 0);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  }, [])
+
   return (
-    <div className={styles.secondSection}>
+    <div className={styles.secondSection} ref={secondSectionRef}>
       {/* tabs */}
-      <ul>
+      <ul className={classNames({ [styles.isFixed]: isFixed })}>
         {tabs.map((tab) => {
           return (
             <li key={tab.key} onClick={() => activate(tab.key)}>
